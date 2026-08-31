b9892458f0e5a100fb90d1481e2d287b0d56443c
serkanyigit5358-creator <serkanyigit5358@gmail.com>
2026-08-31 22:07:58 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev
 export default defineConfig({
   plugins: [react()],
   base: '/Patiyuva/',
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    strictPort: true,
-    allowedHosts: true,
-  },
 })
